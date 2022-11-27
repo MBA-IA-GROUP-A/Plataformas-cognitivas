b7fdf562aeaedf54e539bcebc6b83c3af7e0e764
Vinícius Marcili <vinipetter@gmail.com>
2022-11-27 17:27:31 -0300

    Finish get-data

--- get-data/get-data.py
+++ get-data/get-data.py
@@ -1,6 +1,8 @@
 from google.cloud import bigquery, storage
 from dotenv import load_dotenv
+from datetime import datetime
 import os
+import json
 
 load_dotenv('./.env')
 
@@ -20,6 +22,13 @@ def write_in_storage_as_csv(bucket_name, destination_file_name, data):
     bucket = client.get_bucket(bucket_name)
     blob = bucket.blob(destination_file_name)
     blob.upload_from_string(data.to_csv(), 'text/csv')
+    metadata = {
+        "lines": data.shape[0],
+        "columns": data.shape[1],
+        "date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    }
+    blob_metadata = bucket.blob(destination_file_name.replace(".csv", ".json"))
+    blob_metadata.upload_from_string(json.dumps(metadata), 'application/json')
     print('csv file uploaded in storage')
 
 
