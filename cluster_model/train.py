9c453f6033b16c59912d23c946b69ec93b524e41
Fabiana Masini <masinifabiana@gmail.com>
2022-12-19 00:36:33 -0300

    Kmeans prediction endpoint

--- cluster_model/train.py
+++ cluster_model/train.py
@@ -6,13 +6,12 @@ sys.path.append('normalization')
 from normalization import NormalizedData
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
-from google.cloud import storage
 
 if __name__ == "__main__":
     ms = MinMaxScaler()
     normalized_data = NormalizedData()
     dataset = normalized_data.normalize_data()
-    
+
     target = dataset['Status']
 
     dataset.drop(['Status'], axis=1, inplace=True)
@@ -21,13 +20,18 @@ if __name__ == "__main__":
     cols = X.columns
     X = ms.fit_transform(X)
     X = pd.DataFrame(X, columns=[cols])
-    
+
     kmeans = KMeans(n_clusters=2, random_state=42)
     kmeans.fit(X)
     labels = kmeans.labels_
     correct_labels = sum(target == labels)
-    print("Result: %d out of %d samples were correctly labeled." % (correct_labels, target.size))
-    print('Accuracy score: {0:0.2f}'. format(correct_labels/float(target.size)))
+
+    pred = kmeans.predict(X)
+    print(pred)
+    
+    print(
+        f"Result: {correct_labels} out of {target.size} samples were correctly labeled.")
+    print(f"Accuracy score: {correct_labels/float(target.size)}")
 
     model_name = 'kmeans.joblib'
     dir = 'tmp/models/cluster_model/'
@@ -35,14 +39,3 @@ if __name__ == "__main__":
     if not os.path.isdir(dir):
         os.makedirs(dir)
     joblib.dump(kmeans, filename=f'tmp/models/cluster_model/{model_name}')
-
-    bucket_name = os.getenv('BUCKET_NAME')
-    service_account_json_path = os.getenv('SERVICE_ACCOUNT_JSON_PATH')
-    client = storage.Client.from_service_account_json(service_account_json_path)
-    bucket = client.get_bucket(bucket_name)
-
-    blob = bucket.blob(f'cluster_model/{model_name}')
-    blob.upload_from_filename(f'tmp/models/cluster_model/{model_name}')
-
-    print('Modelo salvo com sucesso!')
-    pass
